7c336035ceb884f31238d2c9505c694fd05bc59b
DANNEX87 <97997540+DANNEX87@users.noreply.github.com>
2026-03-31 20:37:12 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
-  title: 'My Neighborhood HOA',
+  title: 'Timbered Estates',
   description: 'Welcome to Timbered Estates portal',
 }
 
